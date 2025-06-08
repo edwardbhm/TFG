48c0eed336b15a74745294b41d1de59f4355c3ed
edwardbhm <edu98.com@gmail.com>
2025-06-08 19:30:45 +0200

    Error corregido

--- watchtrack/watchtrack/src/app/services/perfil.service.ts
+++ watchtrack/watchtrack/src/app/services/perfil.service.ts
@@ -1,22 +1,33 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PerfilService {
-  private apiUrl = 'http://localhost:8080/usuarios';
+  private apiUrl = 'http://localhost:8080/api/perfil';
 
   constructor(private http: HttpClient) {}
 
+  private getAuthHeaders(): HttpHeaders {
+    const token = localStorage.getItem('token');
+    return new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+  }
+
   getPerfil(): Observable<any> {
     const userId = localStorage.getItem('userId');
-    return this.http.get(`${this.apiUrl}/${userId}`);
+    return this.http.get(`${this.apiUrl}/${userId}`, {
+      headers: this.getAuthHeaders()
+    });
   }
 
   actualizarPerfil(datos: any): Observable<any> {
     const userId = localStorage.getItem('userId');
-    return this.http.put(`${this.apiUrl}/${userId}`, datos);
+    return this.http.put(`${this.apiUrl}/${userId}`, datos, {
+      headers: this.getAuthHeaders()
+    });
   }
 }
